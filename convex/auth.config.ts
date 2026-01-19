6f38c78b9f8ea001ec1ce927ba100211d8f92b31
Le Thanh Son <thsonvt@gmail.com>
2026-01-19 17:45:05 +0800

    chore: clean up auth config

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -7,10 +7,6 @@ export default {
     {
       domain: 'https://accounts.google.com',
       applicationID: "1024516361626-qgg4gagk67u02qhpk969dq0m8rorbl4b.apps.googleusercontent.com",
-    },
-    // {
-    //   domain: 'https://accounts.google.com',
-    //   applicationID: "1024516361626-46hm5hf55m9sgnsplnh16vi0913nndko.apps.googleusercontent.com",
-    // }
+    }
   ],
 };
