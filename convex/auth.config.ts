38d985bf829d3e45a07130f20c6ced68dec63bb3
Le Thanh Son <thsonvt@gmail.com>
2026-01-19 15:46:51 +0800

    Update Google OAuth application ID

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -4,9 +4,13 @@ export default {
       domain: process.env.CONVEX_SITE_URL,
       applicationID: "convex",
     },
+    // {
+    //   domain: 'https://accounts.google.com',
+    //   applicationID: "1024516361626-fnacliisjlmggdl83bpgamdo4uljo244.apps.googleusercontent.com",
+    // },
     {
       domain: 'https://accounts.google.com',
-      applicationID: "1024516361626-fnacliisjlmggdl83bpgamdo4uljo244.apps.googleusercontent.com",
+      applicationID: "1024516361626-46hm5hf55m9sgnsplnh16vi0913nndko.apps.googleusercontent.com",
     }
   ],
 };
